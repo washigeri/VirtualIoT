3b62c00104ceb38a091b5d20148d67b58f0c8f09
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2018-06-16 18:10:10 -0400

    terminé

---
+++ Iot/src/main/java/ClientConnexion.java
@@ -0,0 +1,70 @@
+
+import org.apache.http.client.methods.HttpPost;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ClientConnexion implements Runnable{
+
+    private Socket connexion = null;
+    private PrintWriter writer = null;
+    private BufferedInputStream reader = null;
+    private String commamd = "";
+    private  String restUrl= null;
+    private String username= null;
+    private String password=null;
+    private String jsonData = null;
+
+
+    //Notre liste de commandes. Le serveur nous répondra différemment selon la commande utilisée.
+    private static int count = 0;
+    private String name = "Client-";
+
+    public ClientConnexion(){
+        name += ++count;
+        restUrl="http://localhost:8080/";
+        username="myusername";
+        password="mypassword";
+
+    }
+
+
+    public void run(){
+
+        JSONObject user=new JSONObject();
+        JSONObject data = new JSONObject();
+
+        int ville = randInt(1,500);
+
+        // set Json object
+        data.put("datatype","temperature");
+        data.put("value",String.valueOf(randInt(-30,45)));
+        user.put("location", "ville"+String.valueOf(ville));
+        user.put("deviceID", String.valueOf(ville));
+        user.put("data",data);
+        jsonData=user.toString();
+
+
+        //envoie de la requete en post
+        HttpPostReq httpPostReq=new HttpPostReq();
+        HttpPost httpPost=httpPostReq.createConnectivity(restUrl , username, password);
+        httpPostReq.executeReq( jsonData, httpPost);
+    }
+
+    public static int randInt(int min, int max) {
+
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+        return randomNum;
+    }
+}
---
+++ Iot/src/main/java/HttpPostReq.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class HttpPostReq
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i < 1000; i++){
+            Thread t = new Thread(new ClientConnexion());
+            t.start();
+        }
+    }
+
+    HttpPost createConnectivity(String restUrl, String username, String password)
+    {
+        HttpPost post = new HttpPost(restUrl);
+        String auth=new StringBuffer(username).append(":").append(password).toString();
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        post.setHeader("AUTHORIZATION", authHeader);
+        post.setHeader("Content-Type", "application/json");
+        post.setHeader("Accept", "application/json");
+        post.setHeader("X-Stream" , "true");
+        return post;
+    }
+
+    void executeReq(String jsonData, HttpPost httpPost)
+    {
+        try{
+            executeHttpRequest(jsonData, httpPost);
+        }
+        catch (UnsupportedEncodingException e){
+            System.out.println("error while encoding api url : "+e);
+        }
+        catch (IOException e){
+            System.out.println("ioException occured while sending http request : "+e);
+        }
+        catch(Exception e){
+            System.out.println("exception occured while sending http request : "+e);
+        }
+        finally{
+            httpPost.releaseConnection();
+        }
+    }
+
+    void executeHttpRequest(String jsonData,  HttpPost httpPost)  throws UnsupportedEncodingException, IOException
+    {
+        HttpResponse response=null;
+        String line = "";
+        StringBuffer result = new StringBuffer();
+        httpPost.setEntity(new StringEntity(jsonData));
+        HttpClient client = HttpClientBuilder.create().build();
+        response = client.execute(httpPost);
+        System.out.println("Post parameters : " + jsonData );
+        System.out.println("Response Code : " +response.getStatusLine().getStatusCode());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        while ((line = reader.readLine()) != null){ result.append(line); }
+        System.out.println(result.toString());
+    }
+}
